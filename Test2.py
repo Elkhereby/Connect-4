e014f9a7112199e774f68afb54ff5e7485f4bdaf
Youssef Mohamed <102879208+YoussefMohAttia@users.noreply.github.com>
2024-09-18 20:18:30 +0300

    final update

--- Test2.py
+++ Test2.py
@@ -1,34 +1,41 @@
-from solver import *
+import time
+
+import Solver
 from Board import *
-
-def test_8062(board_state, player_id):
-    if player_id == 1:
-        opponent = 2
-    else:
-        opponent = 1
-    board = Board(board_state)
-    solver = Solver(depth=8,Ai_piece=player_id,player_piece=opponent)
-    col, _ = solver.solve(board, "alphabetapruning")
-    return col
-
-
-if __name__=="__main__":
-
+import numpy as np
+def test_8062(state, player_id):
     board = Board()
-    print(board.current_state, '\n')
-    player_id = 1
-    turn = player_id
-
+    board.load_board(state)
+    solver = Solver.Solver(depth=8)
+    solver.algorithm = "α-βpruning"
+    t = []
     while board.available_places >= 0:
-        if turn != player_id:
-            solver2 = Solver(depth=8, Ai_piece=2, player_piece=1)
-            col, _ = solver2.solve(board, "alphabetapruning")
-            board.add_piece(col, 2)
+        if player_id % 2 == 0:
+            st = time.time()
+            col, value = solver.solve(board)
+            end = time.time()
+            t.append(float(end-st))
+            board.add_piece(col, 1.0)
+            print(col+1)
         else:
-            col = test_8062(board, player_id)
-            board.add_piece(col, player_id)
-        print(board.current_state, '\n')
-        turn += 1
-        turn = turn % 2
 
-
+            col = int(input("Enter Column (1-7)"))
+            while col>7 or col<1:
+                col = int(input("Enter Column (1-7)"))
+            board.add_piece(col, 2.0)
+        print(board)
+        player_id+=1
+    c1 = solver.count_fours(board,1)
+    c2 = solver.count_fours(board,2)
+    return t,c1,c2
+def test_8062(state,player_id):
+    board = Board()
+    board.load_board(state)
+    solver = Solver.Solver(depth=8)
+    solver.algorithm = "α-βpruning"
+    st = time.time()
+    col, value = solver.solve(board)
+    end = time.time()
+    return int(col+1),float(end-st)
+if __name__=="__main__":
+    test_8062(np.zeros((6,7)),0)
